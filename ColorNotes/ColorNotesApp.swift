fa604eb20bf2927507f057de65638563a23f3610
Jun <wnsgur4092@gmail.com>
2023-01-05 14:34:04 +1100

    Initial Commit

---
+++ ColorNotes/ColorNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorNotesApp.swift
+//  ColorNotes
+//
+//  Created by JunHyuk Lim on 5/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
